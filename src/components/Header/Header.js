12ec3364abff6899021eb4f065b9188d01532f3f
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-09-29 21:06:12 +0600

    header icon also added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
+import { faCoffee, faClockFour } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Header.css'
 
 const Header = () => {
   return (
     <div className='headerStyle'>
-      {/* <img src="/public/planner.png" alt=""> </img> */}
-      <h1> Activity in <span>Lockdown time</span></h1>
+      <h1><FontAwesomeIcon icon={faClockFour}></FontAwesomeIcon> Activity in <span>Lockdown time</span></h1>
     </div >
   );
 };
